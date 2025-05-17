b992286695fb2f21e3e40171ffb919906b0829d1
jeyjunior <jeyjuniordg@gmail.com>
2025-05-17 19:58:04 -0300

    Add icones do projeto Add sistema de navegação entre telas

--- FlowCheck.Presentation/MainWindow.xaml.cs
+++ FlowCheck.Presentation/MainWindow.xaml.cs
@@ -13,24 +13,89 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI;           
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
+using FlowCheck.Presentation.View;
 
 namespace FlowCheck.Presentation
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
+        #region Propriedades
+        private const int Largura = 600;
+        private const int Altura = 600;
+        private AppWindow m_AppWindow;
+        private Type paginaAtiva;
+        #endregion
+
+        #region Construtor
         public MainWindow()
         {
             this.InitializeComponent();
+            DefinirPadraoUI();
+            CarregarPagina(typeof(TarefaView), btnTarefa);
         }
+        #endregion
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        #region Eventos
+        private void BtnTarefas_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            CarregarPagina(typeof(TarefaView), btnTarefa);
         }
+        private void BtnAnotacoes_Click(object sender, RoutedEventArgs e)
+        {
+            CarregarPagina(typeof(AnotacaoView), btnAnotacao);
+        }
+        #endregion
+
+        #region Metodos
+        private void DefinirPadraoUI()
+        {
+            m_AppWindow = ObterAppWindowAtual();
+            m_AppWindow.Title = "FlowCheck";
+            m_AppWindow.SetIcon("Assets/flowcheck_icone_24.ico");
+
+            DefinirTamanhoUI();
+            CentralizarUI();
+        }
+        private void DefinirTamanhoUI()
+        {
+            m_AppWindow.Resize(new Windows.Graphics.SizeInt32(Largura, Altura));
+        }
+        private void CentralizarUI()
+        {
+            var displayArea = DisplayArea.GetFromWindowId(m_AppWindow.Id, DisplayAreaFallback.Primary);
+            var centralizacao = new Windows.Graphics.PointInt32
+            {
+                X = displayArea.WorkArea.X + (displayArea.WorkArea.Width - Largura) / 2,
+                Y = displayArea.WorkArea.Y + (displayArea.WorkArea.Height - Altura) / 2
+            };
+            m_AppWindow.Move(centralizacao);
+        }
+        private AppWindow ObterAppWindowAtual()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
+        }
+        private void CarregarPagina(Type pagina, Button botao)
+        {
+            if (paginaAtiva?.Name == pagina.Name)
+                return;
+
+            MainFrame.Navigate(pagina);
+            paginaAtiva = pagina;
+
+            SetActiveButton(botao);
+        }
+        private void SetActiveButton(Button activeButton)
+        {
+            btnTarefa.Opacity = 1;
+            btnAnotacao.Opacity = 1;
+
+            activeButton.Opacity = 0.6;
+        }
+        #endregion
     }
 }
